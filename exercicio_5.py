e5dbbb5c1938d5f52f8e6345153ebab13f8c0abb
matheus.antonio <matheusk42@hotmail.com>
2024-12-27 20:37:29 -0300

    Salvando o quinto exercicio

---
+++ exercicio_5.py
@@ -0,0 +1,7 @@
+#Dada uma lista de emails, remover todos os duplicados.
+
+lista_emails = ['matheusk42@hotmail.com','matheusk42@hotmail.com','joãodascoves@gmai.com','amandasilva@gmail.com','francielegaia@gmail.com']
+
+limpando_duplicados = set(lista_emails)
+
+print(limpando_duplicados)
