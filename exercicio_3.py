6558e3bb0ca5a0791edf7deba4ab3aefb11ffe44
matheus.antonio <matheusk42@hotmail.com>
2024-12-27 20:10:36 -0300

    Salvando o terceiro exercicio

---
+++ exercicio_3.py
@@ -0,0 +1,11 @@
+#Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+
+dict_livros : dict = {
+    "título":"Games of Thrones",
+    "autor" :"Não sei",
+    "publicação": "alguma coisa"
+}
+
+dicionario_items = dict_livros.items()
+
+print(dicionario_items)
