bc938291406c36365ce9af7d95a364a5bdf192df
matheus.antonio <matheusk42@hotmail.com>
2024-12-27 21:31:50 -0300

    Salvando o 13 arquivo

---
+++ exercicio_13.py
@@ -0,0 +1,9 @@
+#Dado um dicionário, criar listas separadas para suas chaves e valores.
+
+dicionario = {"a": 1, "b": 2, "c": 3}
+
+chaves = list(dicionario.keys())
+
+valores = list(dicionario.values())
+
+print(chaves, valores)
